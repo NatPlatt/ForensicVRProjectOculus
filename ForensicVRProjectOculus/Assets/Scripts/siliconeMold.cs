24f810d9b3abcfbde8663608a388fa6ab925dc58
NatPlatt <n.zollinger26@hotmail.com>
2020-12-07 17:32:59 -0700

    added to silicone mold script and edited tweezers

--- ForensicVRProjectOculus/Assets/Scripts/siliconeMold.cs
+++ ForensicVRProjectOculus/Assets/Scripts/siliconeMold.cs
@@ -5,21 +5,23 @@ using UnityEngine;
 public class siliconeMold : MonoBehaviour
 {
     private Animator myAnimator;
-
+    private Animation myAnim;
+    public string myAnimState;
    
     // Start is called before the first frame update
     void Start()
     {
         myAnimator = gameObject.GetComponent<Animator>();
+        myAnim = gameObject.GetComponent<Animation>();
     }
 
     public void RunAnim()
     {
-        myAnimator.SetBool("makeMold", true);
+        myAnim[myAnimState].speed = 1;
     }
 
     public void StopAnim()
     {
-       // myAnimator.keepAnimatorControllerStateOnDisable.
+        myAnim[myAnimState].speed = 0;
     }
 }
