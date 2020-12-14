b342d6530b4424ec154873a9b91f0452662ec8fc
NatPlatt <n.zollinger26@hotmail.com>
2020-12-13 20:09:46 -0700

    adding cotton swab and OBTI test functionality

---
+++ ForensicVRProjectOculus/Assets/Scripts/SetTheBool.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SetTheBool : MonoBehaviour
+{
+    public bool myBool;
+    public UnityEvent trueEvent;
+    public UnityEvent falseEvent;
+    
+    public void SetToTrue()
+    {
+        myBool = true;
+    }
+
+    public void SetToFalse()
+    {
+        myBool = false;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        myBool = other.GetComponent<SetTheBool>().myBool;
+    }
+
+    public void RunTheCorrectEvent()
+    {
+        if (myBool == true)
+        {
+            trueEvent.Invoke();
+        }
+
+        if (myBool == false)
+        {
+            falseEvent.Invoke();
+        }
+    }
+}
